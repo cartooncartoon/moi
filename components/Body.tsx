1879820bf8d54bd5f6c18e972af346f1414890e1
Seah <woolybagel@gmail.com>
2021-04-15 09:07:21 -0400

    added youtube with iframe

--- components/Body.tsx
+++ components/Body.tsx
@@ -15,6 +15,7 @@ const Body: React.SFC<BodyProps> = () => {
                 height: 100vh;
             }
             `}</style>
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/21qNxnCS8WU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,11 +10,38 @@ const Sidebar: React.SFC<SidebarProps> = () => {
         <div className="sidebar">
             <style jsx>{`
             .sidebar {
-                width: 600px;
+                width: 500px;
                 height: 100vh;
                 background: #1a1a1a;
+                padding: 1rem;
+                display: flex;
+                align-items: flex-end;
+            }
+
+            .input {
+                resize: none;
+                border: none;
+                width: 100%;
+                height: 100%;
+                overlay: none;
+                font: inherit;
+                color: #efeff1;
+                background: none;
+                padding: 13px 20px;
+            }
+
+            .input::placeholder {
+                color: #efeff1;
             }
             `}</style>
+            <div style={{
+            maxHeight: 50,
+            width: '100%',
+            overflow: 'hidden', borderRadius: 6, background: 'hsla(0,0%,100%,0.2)'}}>
+            <textarea 
+            className="input"
+            placeholder="What you want to say?"></textarea>
+            </div>
         </div>
     );
 }
